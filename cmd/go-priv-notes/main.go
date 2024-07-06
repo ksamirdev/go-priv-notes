a98e034cba8a6ad1e8b1419d3d0cc0f238202438
Samir <samirdiff@proton.me>
2024-07-06 16:29:47 +0530

    setup simple api server and env variables

--- cmd/go-priv-notes/main.go
+++ cmd/go-priv-notes/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/samocodes/go-priv-notes/env"
+	"github.com/samocodes/go-priv-notes/internal/api"
+)
+
+func init() {
+	env.Load()
+}
+
+type Application struct {
+	config env.Config
+}
+
+func (app *Application) Serve() error {
+	port := app.config.PORT
+
+	log.Printf("🚀 Server listening to port %s", port)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: api.Router(),
+	}
+
+	return srv.ListenAndServe()
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := Application{
+		config: env.DefaultConfig,
+	}
+
+	if err := app.Serve(); err != nil {
+		log.Fatalf("Error starting server: %s\n", err.Error())
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/samocodes/go-priv-notes
 
 go 1.22.5
+
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/joho/godotenv v1.5.1
+)
